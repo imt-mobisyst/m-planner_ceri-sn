03fb92a296c785bf437123d12e3babe077c358e8
Auwal SHEHU <auwal.shehu@imt-nord-europe.fr>
2025-04-29 11:34:08 +0200

    modifying the server code

--- python/flask_server.py
+++ python/flask_server.py
@@ -3,6 +3,10 @@ from flask_cors import CORS
 import sys , os
 import time
 
+import json
+
+import datetime
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'coverage_path_planning')))
 import PT_planner
 # added the way to import
@@ -18,6 +22,8 @@ def receive_json():
     data = request.json
     sequence = list(map(int, planner(data)))
     data['path'] = sequence
+
+    saveToJson(data)
 
     #extra added for loop
     #points_dict = {point["id"]: point for point in data["points"]}
@@ -51,6 +57,11 @@ def planner(data):
 
     sequencePointIds = PT_planner.main('request', request_data = data)
     return  sequencePointIds
+
+def saveToJson(data):
+    JSONfilename =  f"mission_{datetime.datetime.now().strftime('%d-%m-%Y_%H%M%S')}.json"
+    with open(f"coverage_path_planning/json/{JSONfilename}", 'w') as f:
+        json.dump(data, f, indent=4)
 
 if __name__ == "__main__":
     app.run(debug=True)
